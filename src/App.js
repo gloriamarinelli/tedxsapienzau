9a90f7842822bc7e4d4298de702f597cc914bdc5
Gloria Marinelli <gloria.marinelli22@gmail.com>
2023-01-11 16:01:10 +0100

    v.1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import Home from "./components/home";
+import About from "./components/about";
+//import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar />
+    <Footer />
+    <Home />
+    <About />
+    </div>
+  );
+}
+
+export default App;
