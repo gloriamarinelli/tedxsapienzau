a728164d89160093c4f1ea9ed9845ec7038bce24
Gloria Marinelli <gloria.marinelli22@gmail.com>
2023-01-20 19:10:03 +0100

    aggiunto sql database

---
+++ tedxsapienzau.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Gen 20, 2023 alle 19:09
+-- Versione del server: 10.4.21-MariaDB
+-- Versione PHP: 7.4.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tedxsapienzau`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `team`
+--
+
+CREATE TABLE `team` (
+  `nome` varchar(100) NOT NULL,
+  `ruolo` varchar(100) NOT NULL,
+  `foto` varchar(500) NOT NULL,
+  `anno` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
