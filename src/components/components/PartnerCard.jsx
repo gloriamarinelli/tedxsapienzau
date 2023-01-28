d394b72e99a52f3e2229d434c0f66ca776751453
Gloria Marinelli <gloria.marinelli22@gmail.com>
2023-01-28 10:34:01 +0100

    inserito link ai partners

--- src/components/components/PartnerCard.jsx
+++ src/components/components/PartnerCard.jsx
@@ -1,30 +1,22 @@
 import React, { useState, useEffect } from "react";
 
-export default function PartnerCard({ title, subtitle, imgSrc }) {
+export default function PartnerCard({ title, imgSrc, linkSite }) {
 	return (
 		<div className="col-lg-4 col-sm-6 mb-4 d-flex justify-content-center">
 			<div
 				className="portfolio-item"
 				style={{
 					aspectRatio: 1,
-					width: "190px",
+					width: "200px",
 					display: "flex",
 					justifyContent: "center",
 					alignItems: "center",
 					flexDirection: "column",
 				}}
 			>
-				<a className="portfolio-link" data-bs-toggle="modal" href="">
-					<div className="portfolio-hover">
-						<div className="portfolio-hover-content">
-							<i className="fas fa-plus fa-3x"></i>
-						</div>
-					</div>
-					<img className="img-fluid" src={imgSrc} alt="..." />
-				</a>
-				{/* <div className="portfolio-caption">
-					<div className="portfolio-caption-heading">{title}</div>
-				</div> */}
+				<a className="portfolio-link" href={linkSite}>					
+					<img className="img-fluid" src={imgSrc} alt="TedxSponsor"/>
+				</a>				
 			</div>
 		</div>
 	);
