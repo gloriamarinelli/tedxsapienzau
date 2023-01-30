db8ba3faeb9897fc9a3e5d73eb3a3c462d59a631
Gloria Marinelli <gloria.marinelli22@gmail.com>
2023-01-30 15:57:00 +0100

    mysql

--- src/index.js
+++ src/index.js
@@ -7,13 +7,25 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+		  res.send("hello world");
+	 });
+
+
+app.listen(3006, () => {
+  console.log("running server");
+});
