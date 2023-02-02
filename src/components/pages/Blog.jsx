5abda968592a8c5791e400ef68663eda4c5cbc94
Gloria Marinelli <gloria.marinelli22@gmail.com>
2023-02-02 15:50:04 +0100

    fix

--- src/components/pages/Blog.jsx
+++ src/components/pages/Blog.jsx
@@ -42,33 +42,6 @@ export default function Blog() {
           per cambiare il mondo
         </p>
       </div>
-
-      <div class="container">
-        <div class="row">
-          <div class="col-md-4">          
-           
-            <div class="profile-card-2">
-              <img
-                src="http://envato.jayasankarkr.in/code/profile/assets/img/profile-2.jpg"
-                class="img img-responsive"
-              />
-              <div class="profile-name">JOHN DOE</div>
-              <div class="profile-username">@johndoesurname</div>
-              <div class="profile-icons">
-                <a href="#">
-                  <i class="fa fa-facebook"></i>
-                </a>
-                <a href="#">
-                  <i class="fa fa-twitter"></i>
-                </a>
-                <a href="#">
-                  <i class="fa fa-linkedin"></i>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </>
   );
 }
