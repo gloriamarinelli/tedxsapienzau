46c0dba2856f8a73c20811981b7f5082b06a6fbe
Gloria Marinelli <gloria.marinelli22@gmail.com>
2023-02-02 16:07:22 +0100

    blog

--- src/components/pages/Blog.jsx
+++ src/components/pages/Blog.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "../../resources/styles/blogstyle.css";
+import Image from "../images/blogPOST1.png";
+import Image1 from "../images/blog2.jpg";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export default function Blog() {
   return (
@@ -41,6 +44,115 @@ export default function Blog() {
           </strong>{" "}
           per cambiare il mondo
         </p>
+      </div>
+
+      <div class="container">
+        <div class="row no-gutters">
+          <div class="col-xl-6 col-12 mb-5 mb-xl-0">
+            <div class="media media-news">
+              <div class="media-img">
+              <LazyLoadImage
+							src={Image}
+							alt="Post"
+							width="400"
+							height="280"
+						/>
+              </div>
+              <div class="media-body">
+                <span class="media-date">24 dic 2022</span>
+                <h5 class="mt-3 sep">
+                  Natale con TEDxSapienzaU: un albero condiviso e mille idee
+                  appese ai rami
+                </h5>
+                <a
+                  href="blog-post-right-sidebar.html"
+                  class="btn btn-transparent"
+                >
+                  View More
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xl-6 col-12">
+            <div class="media media-news">
+              <div class="media-img">
+              <LazyLoadImage
+							src={Image1}
+							alt="Post"
+							width="400"
+							height="280"
+						/>
+              </div>
+              <div class="media-body">
+                <span class="media-date">16 dic 2022</span>
+                <h5 class="mt-3 sep">
+                  {" "}
+                  Essere artisti nel mondo degli NFT: l’autenticità restituisce
+                  valore ai digital artists
+                </h5>
+                <a
+                  href="blog-post-right-sidebar.html"
+                  class="btn btn-transparent"
+                >
+                  View More
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row no-gutters mt-4">
+          <div class="col-xl-6 col-12 mb-5 mb-xl-0">
+            <div class="media media-news">
+              <div class="media-img">
+              <LazyLoadImage
+							src={Image1}
+							alt="Post"
+							width="400"
+							height="280"
+						/>
+              </div>
+              <div class="media-body">
+                <span class="media-date">9 dic 2022</span>
+                <h5 class="mt-3 sep">
+                  Progresso: antiche istruzioni per l'uso
+                </h5>
+                <a
+                  href="blog-post-right-sidebar.html"
+                  class="btn btn-transparent"
+                >
+                  View More
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xl-6 col-12">
+            <div class="media media-news">
+              <div class="media-img">
+              <LazyLoadImage
+							src={Image1}
+							alt="Post"
+							width="400"
+							height="280"
+						/>
+              </div>
+              <div class="media-body">
+                <span class="media-date">2 dic 2022</span>
+                <h5 class="mt-3 sep">
+                  ProVocAzione: musica, sensualità ed identità. Volosumarte
+                </h5>
+                <a
+                  href="blog-post-right-sidebar.html"
+                  class="btn btn-transparent"
+                >
+                  View More
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
