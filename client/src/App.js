d722cf6bc1eb8ef34df624efdbda0f236eafb324
Simone Teglia <simone.teglia9@gmail.com>
2023-02-04 12:42:00 +0100

    integrazione file dopo cambio struttura

--- client/src/App.js
+++ client/src/App.js
@@ -19,73 +19,73 @@ import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import { Redirect } from "react-router-dom";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>
-        <Navbar />
-        <Outlet />
-        <Footer />
-      </>
-    ),
-    errorElement: (
-      <>
-        <Navbar />
-        <ErrorPage />
-        <Footer />
-      </>
-    ),
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/tedxsapienzau",
-        element: <Home />,
-      },
-      {
-        path: "/partners",
-        element: <Partners />,
-      },
-      {
-        path: "/blog",
-        element: <Blog />,
-      },
-      {
-        path: "/post/:id",
-        element: <BlogPost />,
-      },
-      {
-        path: "/join",
-        element: <Join />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/team2022",
-        element: <Team2022 />,
-      },
-      {
-        path: "/team2023",
-        element: <Team2023 />,
-      },
-      {
-        path: "/edizione2022",
-        element: <Edizione2022 />,
-      },
-      {
-        path: "/write",
-        element: <Write />,
-      },
-    ],
-  },
+	{
+		path: "/",
+		element: (
+			<>
+				<Navbar />
+				<Outlet />
+				<Footer />
+			</>
+		),
+		errorElement: (
+			<>
+				<Navbar />
+				<ErrorPage />
+				<Footer />
+			</>
+		),
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/tedxsapienzau",
+				element: <Home />,
+			},
+			{
+				path: "/partners",
+				element: <Partners />,
+			},
+			{
+				path: "/blog",
+				element: <Blog />,
+			},
+			{
+				path: "/post/:id",
+				element: <BlogPost />,
+			},
+			{
+				path: "/join",
+				element: <Join />,
+			},
+			{
+				path: "/about",
+				element: <About />,
+			},
+			{
+				path: "/team2022",
+				element: <Team2022 />,
+			},
+			{
+				path: "/team2023",
+				element: <Team2023 />,
+			},
+			{
+				path: "/edizione2022",
+				element: <Edizione2022 />,
+			},
+			{
+				path: "/write",
+				element: <Write />,
+			},
+		],
+	},
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
