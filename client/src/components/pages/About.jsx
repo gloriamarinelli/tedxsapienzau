92956b24033f0e51c3270690ebc7f6d1240b2349
Gloria Marinelli <gloria.marinelli22@gmail.com>
2023-02-10 13:05:06 +0100

    fix

--- client/src/components/pages/About.jsx
+++ client/src/components/pages/About.jsx
@@ -238,10 +238,7 @@ const CarouselComponent = () => {
 					src={Image4}
 					alt="First slide"
 				/>
-				<Carousel.Caption>
-					<h3>First slide label</h3>
-					<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-				</Carousel.Caption>
+				
 			</Carousel.Item>
 			<Carousel.Item>
 				<img
@@ -250,10 +247,7 @@ const CarouselComponent = () => {
 					alt="Second slide"
 				/>
 
-				<Carousel.Caption>
-					<h3>Second slide label</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-				</Carousel.Caption>
+				
 			</Carousel.Item>
 			<Carousel.Item>
 				<img
@@ -262,12 +256,7 @@ const CarouselComponent = () => {
 					alt="Third slide"
 				/>
 
-				<Carousel.Caption>
-					<h3>Third slide label</h3>
-					<p>
-						Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-					</p>
-				</Carousel.Caption>
+				
 			</Carousel.Item>
 		</Carousel>
 	);
