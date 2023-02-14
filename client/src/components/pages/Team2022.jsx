971946cbc2cb9ad51fc0c636d1a81cb586e233a4
Gloria Marinelli <gloria.marinelli22@gmail.com>
2023-02-14 12:33:14 +0100

    fix Team 22 and 23 mobile

--- client/src/components/pages/Team2022.jsx
+++ client/src/components/pages/Team2022.jsx
@@ -3,9 +3,12 @@ import ExecutiveTeamCard from "../components/ExecutiveTeamCard";
 import axios from "axios";
 import "../../index.css";
 import { Buffer } from "buffer";
+import { useOutletContext } from "react-router";
+import global from "../../resources/global.json";
 
 export default function Team2022() {
   const [team, setTeam] = useState([]);
+  const [windowSize, setWindowSize] = useOutletContext();
 
   useEffect(() => {
     axios
@@ -18,63 +21,135 @@ export default function Team2022() {
       });
   }, []);
 
-  return (
-    <>
-      <div
-        className="header gradient-background-team2022"
-        style={{
-          padding: "10px",
-          textAlign: "center",
-          color: "#fff",
-          marginBottom: "10px",
-          clipPath: "polygon(0 0, 100% 0, 100% 65%, 0 100%)",
-          height: "40vh",
-          display: "grid",
-          placeItems: "center",
-        }}
-      >
-        <h1
-          className="font-weight-bold mt-5 mb-5"
+  if (windowSize > global.UTILS.MOBILE_WIDTH) {
+    /**
+     * DESKTOP
+     */
+    return (
+      <>
+        <div
+          className="header gradient-background-team2022"
           style={{
-            fontSize: "120px",
-            fontWeight: "bold",
-            fontFamily: "GothamBold",
+            padding: "10px",
+            textAlign: "center",
+            color: "#fff",
+            marginBottom: "10px",
+            clipPath: "polygon(0 0, 100% 0, 100% 65%, 0 100%)",
+            height: "40vh",
+            display: "grid",
+            placeItems: "center",
           }}
         >
-          TEAM 2022
-        </h1>
-      </div>
-
-      <section className="page-section" id="portfolio">
-        <div className="container">
-          <div className="text-center">
-            <h2
-              className="section-heading text-uppercase mb-5"
-              style={{
-                fontFamily: "GothamBold",
-                fontSize: "3em",
-              }}
-            >
-              Team Esecutivo
-            </h2>
-          </div>
-
-          <div className="row gap-5 justify-content-center">
-            {team.map((team) => {
-              const { id, nome, ruolo, image } = team;
-              let base64StringImage = Buffer.from(image, "binary").toString("base64");
-              return (
-                <ExecutiveTeamCard
-                  key={id}
-                  nome={nome}
-                  ruolo={ruolo}
-                  image={base64StringImage}
-                />
-              );
-            })}
-          </div>
+          <h1
+            className="font-weight-bold mt-5 mb-5"
+            style={{
+              fontSize: "120px",
+              fontWeight: "bold",
+              fontFamily: "GothamBold",
+            }}
+          >
+            TEAM 2022
+          </h1>
         </div>
-      </section>
-    </>
-  );
+
+        <section className="page-section" id="portfolio">
+          <div className="container">
+            <div className="text-center">
+              <h2
+                className="section-heading text-uppercase mb-5"
+                style={{
+                  fontFamily: "GothamBold",
+                  fontSize: "3em",
+                }}
+              >
+                Team Esecutivo
+              </h2>
+            </div>
+
+            <div className="row gap-5 justify-content-center">
+              {team.map((team) => {
+                const { id, nome, ruolo, image } = team;
+                let base64StringImage = Buffer.from(image, "binary").toString(
+                  "base64"
+                );
+                return (
+                  <ExecutiveTeamCard
+                    key={id}
+                    nome={nome}
+                    ruolo={ruolo}
+                    image={base64StringImage}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  } else {
+    /**
+     * MOBILE
+     */
+    return (
+      <>
+        <div
+          className="header gradient-background-team2022"
+          style={{
+            padding: "10px",
+            textAlign: "center",
+            color: "#fff",
+            marginBottom: "10px",
+            //clipPath: "polygon(0 0, 100% 0, 100% 65%, 0 100%)",
+            height: "40vh",
+            display: "grid",
+            placeItems: "center",
+          }}
+        >
+          <h1
+            className="font-weight-bold mt-5 mb-5"
+            style={{
+              fontSize: "60px",
+              fontWeight: "bold",
+              fontFamily: "GothamBold",
+            }}
+          >
+            TEAM 2022
+          </h1>
+        </div>
+
+        <section className="page-section" id="portfolio">
+          <div className="container">
+            <div className="text-center">
+              <h3
+                className="section-heading text-uppercase mb-5"
+                style={{
+                  fontFamily: "GothamBold",
+                  fontSize: "3em",
+                }}
+              >
+                Team Esecutivo
+              </h3>
+            </div>
+
+            <div className="row gap-5 justify-content-center">
+              {team.map((team) => {
+                const { id, nome, ruolo, image } = team;
+                let base64StringImage = Buffer.from(image, "binary").toString(
+                  "base64"
+                );
+                return (
+                  <ExecutiveTeamCard
+                    key={id}
+                    nome={nome}
+                    ruolo={ruolo}
+                    image={base64StringImage}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  }
 }
--- client/src/components/pages/Team2023.jsx
+++ client/src/components/pages/Team2023.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-
+import { useOutletContext } from "react-router";
+import global from "../../resources/global.json";
 import "../../index.css";
 import ExecutiveTeamCard from "../components/ExecutiveTeamCard";
 
 export default function Team2023() {
+	const [windowSize, setWindowSize] = useOutletContext();
+
+	if (windowSize > global.UTILS.MOBILE_WIDTH) {
+		/**
+		 * DESKTOP
+		 */
 	return (
 		<>
 			<div
@@ -51,4 +58,56 @@ export default function Team2023() {
 			</section>
 		</>
 	);
-}
+} else {
+    /**
+     * MOBILE
+     */
+	return (
+		<>
+			<div
+				className="header gradient-background-team2023"
+				style={{
+					padding: "10px",
+					textAlign: "center",
+					color: "#fff",
+					marginBottom: "10px",
+					//clipPath: "polygon(0 0, 100% 0, 100% 65%, 0 100%)",
+					height: "40vh",
+					display: "grid",
+					placeItems: "center",
+				}}
+			>
+				<h1
+					className="font-weight-bold mt-5 mb-5"
+					style={{
+						fontSize: "60px",
+						fontWeight: "bold",
+						fontFamily: "GothamBold",
+					}}
+				>
+					TEAM 2023
+				</h1>
+			</div>
+
+			<section className="page-section" id="portfolio">
+				<div className="container">
+					<div className="text-center">
+						<h3
+							className="section-heading text-uppercase mb-5"
+							style={{
+								fontFamily: "GothamBold",
+								fontSize: "3em",
+							}}
+						>
+							Team Esecutivo
+						</h3>
+					</div>
+					<div className="row justify-content-center">
+						
+					</div>
+				</div>
+			</section>
+		</>
+	);
+						}
+					}
