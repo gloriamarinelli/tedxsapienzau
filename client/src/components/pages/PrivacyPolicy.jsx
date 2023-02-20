40cfb9e183eca698534f607762e9584e27769d2e
Gloria Marinelli <gloria.marinelli22@gmail.com>
2023-02-20 18:23:59 +0100

    fix Mobile PrivacypPolicy

--- client/src/components/pages/PrivacyPolicy.jsx
+++ client/src/components/pages/PrivacyPolicy.jsx
@@ -37,7 +37,7 @@ export default function PrivacyPolicy() {
         <div
           style={{
             textAlign: "justify",
-            padding: "100px",
+            padding: "20px",
             fontFamily: "GothamBook",
           }}
         >
