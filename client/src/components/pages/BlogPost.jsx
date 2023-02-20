c98ac777a52fcc74872ded8efded53b54fcfef9c
Gloria Marinelli <gloria.marinelli22@gmail.com>
2023-02-20 13:57:21 +0100

    fix vercel 5

--- client/src/components/pages/BlogPost.jsx
+++ client/src/components/pages/BlogPost.jsx
@@ -9,7 +9,7 @@ import { faFeatherPointed } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { format } from "date-fns";
 import { useNavigate, useParams } from "react-router";
-import { it } from "date-fns";
+import { it } from "date-fns/locale";
 
 export default function BlogPost() {
 	const [blogPost, setBlogPost] = useState([]);
