4814580112561a9cf87ba4a996b0d8f05a3b427a
Simone Teglia <simone.teglia9@gmail.com>
2023-02-21 12:42:20 +0100

    Preview caricamento immagine

--- client/src/components/pages/Write.jsx
+++ client/src/components/pages/Write.jsx
@@ -7,121 +7,122 @@ import "react-quill/dist/quill.snow.css";
 import "../../resources/styles/blogstyle.css";
 
 const Write = () => {
-  const state = useLocation().state;
-  const [value, setValue] = useState(state?.title || "");
-  const [title, setTitle] = useState(state?.desc || "");
-  const [file, setFile] = useState(null);
-  const [author, setAuthor] = useState("");
-  const { currentUser } = useContext(AuthContext);
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (!currentUser) {
-      navigate("/");
-    }
-  }, []);
+	const state = useLocation().state;
+	const [value, setValue] = useState(state?.title || "");
+	const [title, setTitle] = useState(state?.desc || "");
+	const [file, setFile] = useState(null);
+	const [author, setAuthor] = useState("");
+	const [base64Image, setBase64Image] = useState(null);
+	const { currentUser } = useContext(AuthContext);
+	const navigate = useNavigate();
 
-  const upload = async () => {
-    try {
-      const formData = new FormData();
-      formData.append("file", file);
-      const res = await axios.post("/upload", formData);
-      return res.data;
-    } catch (err) {
-      console.log(err);
-    }
-  };
+	useEffect(() => {
+		if (!currentUser) {
+			navigate("/");
+		}
+	}, []);
 
-  const handleClick = async (e) => {
-    e.preventDefault();
+	useEffect(() => {
+		imageToBase64(file).then((res) => setBase64Image(res));
+	}, [file]);
 
-    if (file) {
-      if (value == "" || title == "") {
-        alert("Compila tutti i campi prima di pubblicare l'articolo!");
-        return;
-      }
+	const handleClick = async (e) => {
+		e.preventDefault();
 
-      imageToBase64(file).then((res) => {
-        console.log(res);
-        axios
-          .post("http://localhost:8800/blog", {
-            title: title,
-            description: value,
-            autore: author,
-            image: res,
-            date: new Date(),
-          })
-          .then((res, err) => {
-            console.log(res);
-            navigate("/blog");
-          })
-          .catch((err) => console.error(err));
-      });
-    } else {
-      alert("Aggiungi una foto all'articolo!");
-    }
-  };
+		if (file) {
+			if (value == "" || title == "") {
+				alert("Compila tutti i campi prima di pubblicare l'articolo!");
+				return;
+			}
 
-  return (
-    <div className="container main-container mt-5 mb-5">
-      <div className="content-container">
-        <label htmlFor="title" style={{ fontSize: "30px" }}>
-          Titolo
-        </label>
-        <input
-          id="title"
-          type="text"
-          placeholder="Scrivi qui il titolo"
-          onChange={(e) => setTitle(e.target.value)}
-          className="mb-3"
-          style={{ width: "100%" }}
-        />
-        <div className="editor-container">
-          <ReactQuill
-            className="editor"
-            theme="snow"
-            value={value}
-            onChange={setValue}
-            style={{ height: "400px", marginBottom: "30px" }}
-          />
-        </div>
-      </div>
-      <div className="menu-container">
-        <h3>Completa l'articolo</h3>
-        <input
-          className="file mb-4"
-          type="file"
-          accept="image/*"
-          onChange={(e) => setFile(e.target.files[0])}
-        />
-        <label htmlFor="author">Autore</label>
-        <input
-          id="author"
-          type="text"
-          name="author"
-          onChange={(e) => setAuthor(e.target.value)}
-          className="mb-5"
-        />
-        <div className="buttons">
-          <button className="button" onClick={handleClick}>
-            Publish
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+			imageToBase64(file).then((res) => {
+				console.log(res);
+				axios
+					.post("http://localhost:8800/blog", {
+						title: title,
+						description: value,
+						autore: author,
+						image: res,
+						date: new Date(),
+					})
+					.then((res, err) => {
+						console.log(res);
+						navigate("/blog");
+					})
+					.catch((err) => console.error(err));
+			});
+		} else {
+			alert("Aggiungi una foto all'articolo!");
+		}
+	};
+
+	return (
+		<div className="container main-container mt-5 mb-5">
+			<div className="content-container">
+				<label htmlFor="title" style={{ fontSize: "30px" }}>
+					Titolo
+				</label>
+				<input
+					id="title"
+					type="text"
+					placeholder="Scrivi qui il titolo"
+					onChange={(e) => setTitle(e.target.value)}
+					className="mb-3"
+					style={{ width: "100%" }}
+				/>
+				<div className="editor-container">
+					<ReactQuill
+						className="editor"
+						theme="snow"
+						value={value}
+						onChange={setValue}
+						style={{ height: "400px", marginBottom: "30px" }}
+					/>
+				</div>
+			</div>
+			<div className="menu-container">
+				<h3>Completa l'articolo</h3>
+				<input
+					className="file mb-4"
+					type="file"
+					accept="image/*"
+					onChange={(e) => setFile(e.target.files[0])}
+				/>
+				{base64Image ? (
+					<img src={`data:image/*;base64,${base64Image}`} width="300px" />
+				) : (
+					<></>
+				)}
+				<label htmlFor="author">Autore</label>
+				<input
+					id="author"
+					type="text"
+					name="author"
+					onChange={(e) => setAuthor(e.target.value)}
+					className="mb-5"
+				/>
+				<div className="buttons">
+					<button className="button" onClick={handleClick}>
+						Publish
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Write;
 
 async function imageToBase64(file) {
-  return new Promise((res) => {
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      const base64String = reader.result
-        .replace("data:", "")
-        .replace(/^.+,/, "");
-      res(base64String);
-    };
-    reader.readAsDataURL(file);
-  });
+	if (file)
+		return new Promise((res) => {
+			const reader = new FileReader();
+			reader.onloadend = () => {
+				const base64String = reader.result
+					.replace("data:", "")
+					.replace(/^.+,/, "");
+				res(base64String);
+			};
+			reader.readAsDataURL(file);
+		});
 }
