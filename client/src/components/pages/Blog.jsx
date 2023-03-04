ac8f9f96a3bd035e319784cdaab9f7c23ea26af9
Gloria Marinelli <gloria.marinelli22@gmail.com>
2023-03-04 11:41:03 +0100

    prova background admin

--- client/src/components/pages/Blog.jsx
+++ client/src/components/pages/Blog.jsx
@@ -10,199 +10,198 @@ import { AuthContext } from "../context/authContext";
 import { Pagination } from "react-bootstrap";
 
 export default function Blog() {
-	const [blog, setBlog] = useState([]);
-	const [blogSize, setBlogSize] = useState(0);
-	const [currentPage, setCurrentPage] = useState(1);
-	const [windowSize, setWindowSize] = useOutletContext();
+  const [blog, setBlog] = useState([]);
+  const [blogSize, setBlogSize] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [windowSize, setWindowSize] = useOutletContext();
 
-	const { currentUser } = useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
 
-	const pageSize = 5;
+  const pageSize = 5;
 
-	useEffect(() => {
-		axios
-			.get(global.CONNECTION.ENDPOINT + "blog/count")
-			.then((res) => {
-				setBlogSize(res.data[0].posts);
-				getCurrentPagePosts();
-			})
-			.catch((err) => console.error(err));
-	}, []);
+  useEffect(() => {
+    axios
+      .get(global.CONNECTION.ENDPOINT + "blog/count")
+      .then((res) => {
+        setBlogSize(res.data[0].posts);
+        getCurrentPagePosts();
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
-	useEffect(() => {
-		getCurrentPagePosts();
-	}, [currentPage, blogSize]);
+  useEffect(() => {
+    getCurrentPagePosts();
+  }, [currentPage, blogSize]);
 
-	const getCurrentPagePosts = () => {
-		if (blogSize === 0) return;
-		let start = blogSize - pageSize * currentPage;
-		let end = blogSize - pageSize * (currentPage - 1);
-		console.log(start, end);
-		axios
-			.get(global.CONNECTION.ENDPOINT + `blog?start=${start}&end=${end}`)
-			.then((res) => {
-				setBlog(res.data.reverse());
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	};
+  const getCurrentPagePosts = () => {
+    if (blogSize === 0) return;
+    let start = blogSize - pageSize * currentPage;
+    let end = blogSize - pageSize * (currentPage - 1);
+    console.log(start, end);
+    axios
+      .get(global.CONNECTION.ENDPOINT + `blog?start=${start}&end=${end}`)
+      .then((res) => {
+        setBlog(res.data.reverse());
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
-	const getButtons = () => {
-		let items = [];
+  const getButtons = () => {
+    let items = [];
 
-		for (let number = 1; number <= Math.ceil(blogSize / pageSize); number++) {
-			items.push(
-				<Pagination.Item
-					key={number}
-					active={number === currentPage}
-					onClick={() => {
-						setCurrentPage(number);
-						window.scrollTo(0, 300);
-					}}
-				>
-					{number}
-				</Pagination.Item>
-			);
-		}
+    for (let number = 1; number <= Math.ceil(blogSize / pageSize); number++) {
+      items.push(
+        <Pagination.Item
+          key={number}
+          active={number === currentPage}
+          onClick={() => {
+            setCurrentPage(number);
+            window.scrollTo(0, 300);
+          }}
+        >
+          {number}
+        </Pagination.Item>
+      );
+    }
 
-		return (
-			<div
-				style={{
-					width: "100%",
-					display: "flex",
-					color: "black",
-					justifyContent: "center",
-					margin: "20px 0 0 0 ",
-				}}
-			>
-				<Pagination>
-					<Pagination.First />
-					{items}
-					<Pagination.Last />
-				</Pagination>
-			</div>
-		);
-	};
+    return (
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          margin: "20px 0 0 0 ",
+        }}
+      >
+        <Pagination>
+          <Pagination.First />
+          {items}
+          <Pagination.Last />
+        </Pagination>
+      </div>
+    );
+  };
 
-	if (windowSize > global.UTILS.MOBILE_WIDTH) {
-		/**
-		 * DESKTOP
-		 */
-		return (
-			<>
-				<div
-					className="header-blog"
-					style={{
-						textAlign: "left",
-						color: "white",
-						display: "grid",
-						clipPath: "polygon(0 0, 100% 0, 100% 65%, 0 100%)",
-						height: "50vh",
-					}}
-				>
-					<h1
-						className="font-weight-bold mt-5"
-						style={{
-							fontSize: "70px",
-							fontWeight: "bold",
-							marginLeft: "30px",
-						}}
-					>
-						IDEAS WORTH SPREADING
-					</h1>
-					<p
-						style={{
-							fontSize: "50px",
-							fontStyle: "italic",
-							marginLeft: "30px",
-						}}
-					>
-						Le{" "}
-						<strong
-							style={{
-								color: "#EB0028",
-							}}
-						>
-							idee
-						</strong>{" "}
-						per cambiare il mondo
-					</p>
-				</div>
-				{getButtons()}
-				{blog.map((blog) => {
-					const { id, titolo, image, data } = blog;
-					return (
-						<BlogCard
-							key={id}
-							titolo={titolo}
-							image={image}
-							data={data}
-							id={id}
-						/>
-					);
-				})}
-				{getButtons()}
-			</>
-		);
-	} else {
-		/**
-		 * MOBILE
-		 */
-		return (
-			<>
-				<div
-					className="header-blog"
-					style={{
-						textAlign: "left",
-						color: "white",
-						display: "grid",
-						//clipPath: "polygon(0 0, 100% 0, 100% 65%, 0 100%)",
-						height: "50vh",
-					}}
-				>
-					<h1
-						className="font-weight-bold mt-5"
-						style={{
-							fontSize: "45px",
-							fontWeight: "bold",
-							marginLeft: "30px",
-						}}
-					>
-						IDEAS WORTH SPREADING
-					</h1>
-					<p
-						style={{
-							fontSize: "25px",
-							fontStyle: "italic",
-							marginLeft: "30px",
-						}}
-					>
-						Le{" "}
-						<strong
-							style={{
-								color: "#EB0028",
-							}}
-						>
-							idee
-						</strong>{" "}
-						per cambiare il mondo
-					</p>
-				</div>
-				{blog.map((blog) => {
-					const { id, titolo, image, data } = blog;
-					return (
-						<BlogCard
-							key={id}
-							titolo={titolo}
-							image={image}
-							data={data}
-							id={id}
-						/>
-					);
-				})}
-				{getButtons()}
-			</>
-		);
-	}
+  if (windowSize > global.UTILS.MOBILE_WIDTH) {
+    /**
+     * DESKTOP
+     */
+    return (
+      <>
+        <div
+          className="header-blog"
+          style={{
+            textAlign: "left",
+            color: "white",
+            display: "grid",
+            clipPath: "polygon(0 0, 100% 0, 100% 65%, 0 100%)",
+            height: "50vh",
+          }}
+        >
+          <h1
+            className="font-weight-bold mt-5"
+            style={{
+              fontSize: "70px",
+              fontWeight: "bold",
+              marginLeft: "30px",
+            }}
+          >
+            IDEAS WORTH SPREADING
+          </h1>
+          <p
+            style={{
+              fontSize: "50px",
+              fontStyle: "italic",
+              marginLeft: "30px",
+            }}
+          >
+            Le{" "}
+            <strong
+              style={{
+                color: "#EB0028",
+              }}
+            >
+              idee
+            </strong>{" "}
+            per cambiare il mondo
+          </p>
+        </div>
+       
+        {blog.map((blog) => {
+          const { id, titolo, image, data } = blog;
+          return (
+            <BlogCard
+              key={id}
+              titolo={titolo}
+              image={image}
+              data={data}
+              id={id}
+            />
+          );
+        })}
+        {getButtons()}
+      </>
+    );
+  } else {
+    /**
+     * MOBILE
+     */
+    return (
+      <>
+        <div
+          className="header-blog"
+          style={{
+            textAlign: "left",
+            color: "white",
+            display: "grid",
+            //clipPath: "polygon(0 0, 100% 0, 100% 65%, 0 100%)",
+            height: "50vh",
+          }}
+        >
+          <h1
+            className="font-weight-bold mt-5"
+            style={{
+              fontSize: "45px",
+              fontWeight: "bold",
+              marginLeft: "30px",
+            }}
+          >
+            IDEAS WORTH SPREADING
+          </h1>
+          <p
+            style={{
+              fontSize: "25px",
+              fontStyle: "italic",
+              marginLeft: "30px",
+            }}
+          >
+            Le{" "}
+            <strong
+              style={{
+                color: "#EB0028",
+              }}
+            >
+              idee
+            </strong>{" "}
+            per cambiare il mondo
+          </p>
+        </div>
+        {blog.map((blog) => {
+          const { id, titolo, image, data } = blog;
+          return (
+            <BlogCard
+              key={id}
+              titolo={titolo}
+              image={image}
+              data={data}
+              id={id}
+            />
+          );
+        })}
+        {getButtons()}
+      </>
+    );
+  }
 }
--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -12,87 +12,87 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 
 export default function Login() {
-	const { currentUser } = useContext(AuthContext);
-	const { logout } = useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
+  const { logout } = useContext(AuthContext);
 
-	return (
-		<div
-			style={{
-				height: `calc(100vh - ${global.UTILS.NAV_HEIGHT} - ${global.UTILS.SOCIAL_NAV_HEIGHT})`,
-			}}
-			className="login-main-container"
-		>
-			<div className="login-container">
-				<LazyLoadImage src={Image} alt="logo" width="" height="50" />
-				<h1
-					className="mb-4 mt-4"
-					style={{
-						fontFamily: "GothamBold",
-					}}
-				>
-					{currentUser ? `Benvenuto, ${currentUser.username}` : "LOGIN"}
-				</h1>
-				{currentUser ? <div style={{ width: "80%" }}></div> : LoginForm()}
-			</div>
-		</div>
-	);
+  return (
+    <div
+      style={{
+        height: `calc(100vh - ${global.UTILS.NAV_HEIGHT} - ${global.UTILS.SOCIAL_NAV_HEIGHT})`,
+      }}
+      className="login-main-container"
+    >
+      <div className="login-container">
+        <LazyLoadImage src={Image} alt="logo" width="" height="50" />
+        <h1
+          className="mb-4 mt-4"
+          style={{
+            fontFamily: "GothamBold",
+          }}
+        >
+          {currentUser ? `Benvenuto, ${currentUser.username}` : "LOGIN"}
+        </h1>
+        {currentUser ? <div style={{ width: "80%" }}></div> : LoginForm()}
+      </div>
+    </div>
+  );
 }
 
 const LoginForm = () => {
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
-	const [error, setError] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-	const { login } = useContext(AuthContext);
+  const { login } = useContext(AuthContext);
 
-	const errorMessageRef = useRef();
+  const errorMessageRef = useRef();
 
-	const handleSubmit = async (event) => {
-		event.preventDefault();
-		await login({ username: username, password: password })
-			.then((res) => console.log(res))
-			.catch((err) => {
-				setError(err.response.data);
-				console.log(err);
-				errorMessageRef.current.classList.remove("error-animation");
-				void errorMessageRef.current.offsetWidth;
-				errorMessageRef.current.classList.add("error-animation");
-			});
-	};
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await login({ username: username, password: password })
+      .then((res) => console.log(res))
+      .catch((err) => {
+        setError(err.response.data);
+        console.log(err);
+        errorMessageRef.current.classList.remove("error-animation");
+        void errorMessageRef.current.offsetWidth;
+        errorMessageRef.current.classList.add("error-animation");
+      });
+  };
 
-	return (
-		<>
-			<form onSubmit={handleSubmit} style={{ width: "100%" }}>
-				<div style={{ display: "flex", flexDirection: "column" }}>
-					<label htmlFor="username">Username:</label>
-					<input
-						type="text"
-						id="username"
-						className="mb-4"
-						placeholder=""
-						value={username}
-						onChange={(event) => setUsername(event.target.value)}
-					/>
-					<label htmlFor="password">Password:</label>
-					<input
-						type="password"
-						id="password"
-						className="mb-5"
-						value={password}
-						onChange={(event) => setPassword(event.target.value)}
-					/>
-					<button type="submit" className="button" style={{ width: "100%" }}>
-						Login
-					</button>
-					<span
-						className="error-message error-animation mt-3"
-						ref={errorMessageRef}
-						style={{ display: error ? "initial" : "none" }}
-					>
-						<FontAwesomeIcon icon={faCircleXmark} /> {error}
-					</span>
-				</div>
-			</form>
-		</>
-	);
+  return (
+    <>
+      <form onSubmit={handleSubmit} style={{ width: "100%" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            className="mb-4"
+            placeholder=""
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            className="mb-5"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <button type="submit" className="button" style={{ width: "100%" }}>
+            Login
+          </button>
+          <span
+            className="error-message error-animation mt-3"
+            ref={errorMessageRef}
+            style={{ display: error ? "initial" : "none" }}
+          >
+            <FontAwesomeIcon icon={faCircleXmark} /> {error}
+          </span>
+        </div>
+      </form>
+    </>
+  );
 };
