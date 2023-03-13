aa1cc362d9aa3104f39f23eebfd335e1d70e1c1f
Gloria Marinelli <gloria.marinelli22@gmail.com>
2023-03-13 21:23:26 +0100

    sitemap

---
+++ client/sitemap.js
@@ -0,0 +1,15 @@
+// sitemap.js
+
+import Generator from 'react-router-sitemap-generator';
+import Router from '../api/routes'; //import your react router component
+
+const generator = new Generator(
+  'https://react-router-sitemap-generator.com',
+  Router(),
+  {
+    lastmod: new Date().toISOString().slice(0, 10),
+    changefreq: 'monthly',
+    priority: 0.8,
+  }
+);
+generator.save('public/sitemap.xml');
