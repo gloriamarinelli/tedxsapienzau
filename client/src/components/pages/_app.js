860939889de6efc65a94d0ab4506cfa2fb7e0789
Gloria Marinelli <gloria.marinelli22@gmail.com>
2023-04-03 19:28:47 +0200

    analytics vercel

---
+++ client/src/components/pages/_app.js
@@ -0,0 +1,10 @@
+import { Analytics } from "@vercel/analytics/react";
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
+}
+export default MyApp;
