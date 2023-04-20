b047d35f5db179ade736fec4126414a7c4001074
Gloria Marinelli <gloria.marinelli22@gmail.com>
2023-04-20 16:01:16 +0200

    vincitori awards 2023

--- client/src/components/pages/Awards.jsx
+++ client/src/components/pages/Awards.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import Image2 from "../images/nina.jpg";
+import Image1 from "../images/ilaria.jpg";
+import Image5 from "../images/riccardo.jpg";
+import Image4 from "../images/myriam.jpg";
+import Image3 from "../images/matteo.jpg";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendar, faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import "../../index.css";
@@ -40,7 +46,7 @@ export default function Awards() {
               size="2x"
             />
             <p className="col-8">18 Aprile 2023</p>
-          
+
             <div className="row mt-3">
               <a
                 href="https://www.eventbrite.it/e/biglietti-awards-by-tedxsapienzau-514452590547"
@@ -97,6 +103,61 @@ export default function Awards() {
           , vengono premiati sul palco della finale e hanno l’occasione di
           esibirsi durante l’evento <strong>TEDxSapienzaU 2023</strong>!
         </p>
+      </div>
+
+      <h1
+        className="font-weight-bold mt-5 mb-3"
+        style={{
+          fontSize: "45px",
+          textAlign: "center",
+          fontWeight: "bold",
+        }}
+      >
+        I VINCITORI TEDxSAPIENZAU AWARDS 2023
+      </h1>
+      <h3
+        className="font-weight-bold mt-5 mb-3"
+        style={{
+          fontSize: "45px",
+          textAlign: "center",
+        }}
+      >
+        Artist
+      </h3>
+      <div class="container mt-5 mb-5">
+        <div class="row justify-content-center" style={{ gap: "20px" }}>
+          <div class="col-4">
+            {" "}
+            <LazyLoadImage src={Image1} alt="TedxSapienzaU" height="400" />
+          </div>
+          <div class="col-4">
+            {" "}
+            <LazyLoadImage src={Image2} alt="TedxSapienzaU" height="400" />
+          </div>
+        </div>
+      </div>
+
+      <h3
+        className="font-weight-bold mt-5 mb-3"
+        style={{
+          fontSize: "45px",
+          textAlign: "center",
+        }}
+      >
+        Speaker
+      </h3>
+      <div class="container mt-5 mb-5">
+        <div class="row" style={{ gap: "20px" }}>
+          <div class="col">
+            <LazyLoadImage src={Image3} alt="TedxSapienzaU" height="400" />
+          </div>
+          <div class="col">
+            <LazyLoadImage src={Image4} alt="TedxSapienzaU" height="400" />
+          </div>
+          <div class="col">
+            <LazyLoadImage src={Image5} alt="TedxSapienzaU" height="400" />
+          </div>
+        </div>
       </div>
     </section>
   );
