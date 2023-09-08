5afe49b54c3921fe1b5a0f83ea33c937400e10b8
Gloria Marinelli <gloria.marinelli123@gmail.com>
2023-09-08 20:02:30 +0200

    Revert "update"
    This reverts commit 5782dce14e044c81fa7b4818f8dcd7a27f5b3395.

--- client/src/components/components/CountdownTimer.js
+++ client/src/components/components/CountdownTimer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "../../resources/styles/RFWstyle.css";
 
 function CountdownTimer() {
--- client/src/components/context/authContext.js
+++ client/src/components/context/authContext.js
@@ -1,72 +1,70 @@
-import React, { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
 import global from "../../resources/global.json";
 import axios from "axios";
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(
-    JSON.parse(localStorage.getItem("user")) || null
-  );
+	const [currentUser, setCurrentUser] = useState(
+		JSON.parse(localStorage.getItem("user")) || null
+	);
 
-  const [currentToken, setCurrentToken] = useState(
-    JSON.parse(localStorage.getItem("token")) || null
-  );
+	const [currentToken, setCurrentToken] = useState(
+		JSON.parse(localStorage.getItem("token")) || null
+	);
 
-  useEffect(() => {
-    localStorage.setItem("user", JSON.stringify(currentUser));
-  }, [currentUser]);
+	useEffect(() => {
+		localStorage.setItem("user", JSON.stringify(currentUser));
+	}, [currentUser]);
 
-  useEffect(() => {
-    localStorage.setItem("token", JSON.stringify(currentToken));
-  }, [currentToken]);
+	useEffect(() => {
+		localStorage.setItem("token", JSON.stringify(currentToken));
+	}, [currentToken]);
 
-  const login = async (inputs) => {
-    return new Promise((res, rej) => {
-      axios
-        .post(global.CONNECTION.ENDPOINT + "auth/login", inputs)
-        .then((result) => {
-          setCurrentUser(result.data.userData);
-          setCurrentToken(result.data.token);
-          res(result);
-        })
-        .catch((err) => {
-          rej(err);
-        });
-    });
-  };
+	const login = async (inputs) => {
+		return new Promise((res, rej) => {
+			axios
+				.post(global.CONNECTION.ENDPOINT + "auth/login", inputs)
+				.then((result) => {
+					setCurrentUser(result.data.userData);
+					setCurrentToken(result.data.token);
+					res(result);
+				})
+				.catch((err) => {
+					rej(err);
+				});
+		});
+	};
 
-  const logout = async (inputs) => {
-    await axios.post(global.CONNECTION.ENDPOINT + "auth/logout");
-    setCurrentUser(null);
-    setCurrentToken(null);
-    window.location.href = "/";
-  };
+	const logout = async (inputs) => {
+		await axios.post(global.CONNECTION.ENDPOINT + "auth/logout");
+		setCurrentUser(null);
+		setCurrentToken(null);
+		window.location.href = "/";
+	};
 
-  const isUserLoggedIn = async (token) => {
-    return new Promise((res, rej) => {
-      axios
-        .post(global.CONNECTION.ENDPOINT + "auth/isUserLoggedIn", {
-          headers: {
-            "x-auth-token": token,
-          },
-        })
-        .then((result) => {
-          res(result);
-        })
-        .catch((error) => {
-          rej(error);
-        });
-    });
-  };
+	const isUserLoggedIn = async (token) => {
+		return new Promise((res, rej) => {
+			axios
+				.post(global.CONNECTION.ENDPOINT + "auth/isUserLoggedIn", {
+					headers: {
+						"x-auth-token": token,
+					},
+				})
+				.then((result) => {
+					res(result);
+				})
+				.catch((error) => {
+					rej(error);
+				});
+		});
+	};
 
-  return (
-    <>
-      <AuthContext.Provider
-        value={{ currentUser, currentToken, login, logout, isUserLoggedIn }}
-      >
-        {children}
-      </AuthContext.Provider>
-    </>
-  );
+	return (
+		<AuthContext.Provider
+			value={{ currentUser, currentToken, login, logout, isUserLoggedIn }}
+		>
+			{children}
+		</AuthContext.Provider>
+	);
 };
