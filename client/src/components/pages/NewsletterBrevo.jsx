5c5343e7f2070daf227ff90725f1206ef7013a2e
Gloria Marinelli <gloria.marinelli123@gmail.com>
2023-10-08 12:45:43 +0200

    newsletter: changes

--- client/src/components/pages/NewsletterBrevo.jsx
+++ client/src/components/pages/NewsletterBrevo.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useLocation, Link } from "react-router-dom";
+import React from "react";
+import { useLocation } from "react-router-dom";
 import { useOutletContext } from "react-router";
 import global from "../../resources/global.json";
 import Iframe from "react-iframe";
@@ -51,38 +51,40 @@ export default function NewsletterBrevo() {
     /**
      * MOBILE
      */
-    return <>
-    <div
-      className="gradient-background"
-      style={{
-        backgroundColor: "red",
-        textAlign: "left",
-        display: "grid",
-        height: "40vh",
-        color: "#fff",
-        clipPath: "polygon(0 0, 100% 0, 100% 65%, 0 100%)",
-        placeItems: "center",
-        marginTop: global.UTILS.SOCIAL_NAV_HEIGHT,
-      }}
-    >
-      <h1
-        className="font-weight-bold mt-5 mb-5 text-center"
-        style={{
-          fontSize: "35px",
-          fontWeight: "bold",
-          fontFamily: "GothamBold",
-        }}
-      >
-        Resta aggiornato <br />
-        Iscriviti anche tu alla newsletter!{" "}
-      </h1>
-    </div>
+    return (
+      <>
+        <div
+          className="gradient-background"
+          style={{
+            backgroundColor: "red",
+            textAlign: "left",
+            display: "grid",
+            height: "40vh",
+            color: "#fff",
+            clipPath: "polygon(0 0, 100% 0, 100% 65%, 0 100%)",
+            placeItems: "center",
+            marginTop: global.UTILS.SOCIAL_NAV_HEIGHT,
+          }}
+        >
+          <h1
+            className="font-weight-bold mt-5 mb-5 text-center"
+            style={{
+              fontSize: "35px",
+              fontWeight: "bold",
+              fontFamily: "GothamBold",
+            }}
+          >
+            Resta aggiornato <br />
+            Iscriviti anche tu alla newsletter!{" "}
+          </h1>
+        </div>
 
-    <Iframe
-      width="100%"
-      height="600"
-      src="https://31dfcf3b.sibforms.com/serve/MUIFANTb2PgDRsFNib3WYsB0XikTNHLNqorfqEr1y3O0yDmb2V_mpbIggi5dgSHaVxdd0hKFu2ulrerVG35KEmlZgr7boTiHk5Un9lPyBHaucqk-KkSev4b0mEzcM7KqfVTmJfrt43ut2jt_JfywGkiZ9yK02Ryyc2k5zjCbLqWiv2_UFWKF5WQScWWJYrjd_xQQjfYzc6Te6S4n"
-    ></Iframe>
-  </>
+        <Iframe
+          width="100%"
+          height="600"
+          src="https://31dfcf3b.sibforms.com/serve/MUIFAIg0zbBkaxs7zap8K5qhO-YqDD2876NMmonXINEkn4KLE0VtfCtJArVGmFvPqaXc2DhA_UbGoPqH3CEaQ-r0w-gt7-OVQRTIUQHEhsrF3j5g9v4YSLoucqrcQdVxXVwTata7SopA7Eb2Lc7oEY5UkEJXasZoStXYULrDrNn873DqrvdD0s223SODBcKBldaLKE-mlH9r1GeW"
+        ></Iframe>
+      </>
+    );
   }
 }
