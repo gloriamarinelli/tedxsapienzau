7680db25b63ca294893fd8b9651ee083fcb689bb
stedagne <stedagnese@gmail.com>
2024-04-08 18:12:06 +0200

    join us fix mobile

--- client/src/components/pages/Join.jsx
+++ client/src/components/pages/Join.jsx
@@ -3,7 +3,7 @@ import { useLocation, Link } from "react-router-dom";
 import { useOutletContext } from "react-router";
 import global from "../../resources/global.json";
 import Iframe from "react-iframe";
-import backgroundVolunteers from "../images/volunteers.webp"
+import backgroundVolunteers from "../images/volunteers.webp";
 
 export default function Join() {
   const state = useLocation().state;
@@ -87,56 +87,55 @@ export default function Join() {
               }}
             >
               Le candidature per far parte del team sono chiuse, ma non per
-            sempre.<br></br> Compila il form qui sotto per essere contattato
-            quando riapriremo le candidature!
+              sempre.<br></br> Compila il form qui sotto per essere contattato
+              quando riapriremo le candidature!
             </h1>
           </div>
         </section>
         <section
-        style={{
-          display: windowSize > global.UTILS.TABLET_WIDTH ? "flex" : "flow",
-          justifyContent: "center",
-          alignItems: "center",
-          height:
-            windowSize > global.UTILS.TABLET_WIDTH
-              ? `calc(90vh - ${global.UTILS.NAV_HEIGHT})`
-              : `calc(130vh - ${global.UTILS.NAV_HEIGHT})`,
-          width: "100vw",
-          backgroundColor: "#000",
-          padding: global.UTILS.BENTO_BOX_PADDING,
-        }}
-      >
-        
-        <div
-          id="right-container"
           style={{
-            width: "100%",
-            height: windowSize > global.UTILS.TABLET_WIDTH ? "100%" : "100%",
-            padding: global.UTILS.BENTO_BOX_PADDING,
-            borderRadius: global.UTILS.BENTO_BOX_PADDING,
-            backgroundImage: `url(${backgroundVolunteers})`, 
-            backgroundSize: "cover", 
-            backgroundPosition: "center", 
-            display: "flex",
-            marginBottom:
-              windowSize > global.UTILS.TABLET_WIDTH ? "0px" : "200vw",
+            display: windowSize > global.UTILS.TABLET_WIDTH ? "flex" : "flow",
             justifyContent: "center",
-            alignItems: "flex-end",
-            fontFamily: "Fira Sans Extra Condensed, sans-serif",
-            position: "relative",
-            overflow: "hidden",
+            alignItems: "center",
+            height:
+              windowSize > global.UTILS.TABLET_WIDTH
+                ? `calc(90vh - ${global.UTILS.NAV_HEIGHT})`
+                : `calc(130vh - ${global.UTILS.NAV_HEIGHT})`,
+            width: "100vw",
+            backgroundColor: "#000",
+            padding: global.UTILS.BENTO_BOX_PADDING,
           }}
         >
-          <Iframe
-            width="100%"
-            height="100%"
-            src="https://31dfcf3b.sibforms.com/serve/MUIFAGmJoAVv_QJEw7JN7zjN0ucQ52-6hwypIyMu_k-q54TIIoobWKD0giKOQ8S7YqLCXNB3yUuLxFNlXbl3W8gMyt3XtVqxLzpyj45GmokToGpsRe0GXDv0pHrPyQRx3hid7ViTQxx5IMWLrIE4jxASnL6FsD8KljM7qf4u2BhDOQDyfN8DcMvh8E73bfRYFULDqOzCSc1ThILU"
-            frameborder="0"
-            allowfullscreen
-            style="display: block;margin-top: 0;margin-left: auto;margin-right: auto;max-width: 100%;"
-          ></Iframe>
-        </div>
-      </section>
+          <div
+            id="right-container"
+            style={{
+              width: "100%",
+              height: "100%",
+              padding: global.UTILS.BENTO_BOX_PADDING,
+              borderRadius: global.UTILS.BENTO_BOX_PADDING,
+              backgroundImage: `url(${backgroundVolunteers})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              display: "flex",
+              marginBottom:
+                windowSize > global.UTILS.TABLET_WIDTH ? "0px" : "200vw",
+              justifyContent: "center",
+              alignItems: "flex-end",
+              fontFamily: "Fira Sans Extra Condensed, sans-serif",
+              position: "relative",
+              overflow: "hidden",
+            }}
+          >
+            <Iframe
+              width="100%"
+              height="100%"
+              src="https://31dfcf3b.sibforms.com/serve/MUIFAGmJoAVv_QJEw7JN7zjN0ucQ52-6hwypIyMu_k-q54TIIoobWKD0giKOQ8S7YqLCXNB3yUuLxFNlXbl3W8gMyt3XtVqxLzpyj45GmokToGpsRe0GXDv0pHrPyQRx3hid7ViTQxx5IMWLrIE4jxASnL6FsD8KljM7qf4u2BhDOQDyfN8DcMvh8E73bfRYFULDqOzCSc1ThILU"
+              frameborder="0"
+              allowfullscreen
+              style="display: block;margin-top: 0;margin-left: auto;margin-right: auto;max-width: 100%;"
+            ></Iframe>
+          </div>
+        </section>
       </>
     );
   } else {
@@ -184,6 +183,7 @@ export default function Join() {
         <section
           style={{
             backgroundColor: global.COLORS.NERO, //! Cambiare colore
+            height: "20vh",
             padding: "10px",
             fontFamily: "Fira Sans Extra Condensed, sans-serif",
             placeItems: "center",
@@ -194,7 +194,7 @@ export default function Join() {
             className="header"
             style={{
               width: "98%",
-              height: "100%",
+              height: "80%",
               padding: global.UTILS.BENTO_BOX_PADDING,
               borderRadius: global.UTILS.BENTO_BOX_PADDING,
               backgroundColor: "#191919",
@@ -207,64 +207,57 @@ export default function Join() {
             <h1
               className="font-weight-bold"
               style={{
-                fontSize: "30px",
+                fontSize: "20px",
                 fontWeight: "bold",
                 color: global.COLORS.BIANCO,
               }}
             >
               Le candidature per far parte del team sono chiuse, ma non per
-            sempre.<br></br> Compila il form qui sotto per essere contattato
-            quando riapriremo le candidature!
+              sempre.<br></br> Compila il form qui sotto per essere contattato
+              quando riapriremo le candidature!
             </h1>
           </div>
         </section>
         <section
-        style={{
-          display: windowSize > global.UTILS.TABLET_WIDTH ? "flex" : "flow",
-          justifyContent: "center",
-          alignItems: "center",
-          height:
-            windowSize > global.UTILS.TABLET_WIDTH
-              ? `calc(90vh - ${global.UTILS.NAV_HEIGHT})`
-              : `calc(100vh - ${global.UTILS.NAV_HEIGHT})`,
-          width: "100vw",
-          backgroundColor: "#000",
-          padding: global.UTILS.BENTO_BOX_PADDING,
-        }}
-      >
-        
-        <div
-          id="right-container"
           style={{
-            width: "100%",
-            height: windowSize > global.UTILS.TABLET_WIDTH ? "100%" : "100%",
-            padding: global.UTILS.BENTO_BOX_PADDING,
-            borderRadius: global.UTILS.BENTO_BOX_PADDING,
-            backgroundImage: `url(${backgroundVolunteers})`, 
-            backgroundSize: "cover", 
-            backgroundPosition: "center", 
             display: "flex",
-            
-            
             justifyContent: "center",
-            alignItems: "flex-end",
-            fontFamily: "Fira Sans Extra Condensed, sans-serif",
-            position: "relative",
-            overflow: "hidden",
+            alignItems: "center",
+            height: `calc(100vh - ${global.UTILS.NAV_HEIGHT})`,
+            width: "100vw",
+            backgroundColor: "#000",
+            padding: global.UTILS.BENTO_BOX_PADDING,
           }}
         >
-          <Iframe
-            width="100%"
-            height="100%"
-            src="https://31dfcf3b.sibforms.com/serve/MUIFAGmJoAVv_QJEw7JN7zjN0ucQ52-6hwypIyMu_k-q54TIIoobWKD0giKOQ8S7YqLCXNB3yUuLxFNlXbl3W8gMyt3XtVqxLzpyj45GmokToGpsRe0GXDv0pHrPyQRx3hid7ViTQxx5IMWLrIE4jxASnL6FsD8KljM7qf4u2BhDOQDyfN8DcMvh8E73bfRYFULDqOzCSc1ThILU"
-            frameborder="0"
-            allowfullscreen
-            style="display: block;margin-top: 0;margin-left: auto;margin-right: auto;max-width: 100%;"
-          ></Iframe>
-        </div>
-      </section>
+          <div
+            id="right-container"
+            style={{
+              width: "100%",
+              height: "100%",
+              padding: global.UTILS.BENTO_BOX_PADDING,
+              borderRadius: global.UTILS.BENTO_BOX_PADDING,
+              backgroundImage: `url(${backgroundVolunteers})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              display: "flex",
 
-       
+              justifyContent: "center",
+              alignItems: "flex-end",
+              fontFamily: "Fira Sans Extra Condensed, sans-serif",
+              position: "relative",
+              overflow: "hidden",
+            }}
+          >
+            <Iframe
+              width="100%"
+              height="100%"
+              src="https://31dfcf3b.sibforms.com/serve/MUIFAGmJoAVv_QJEw7JN7zjN0ucQ52-6hwypIyMu_k-q54TIIoobWKD0giKOQ8S7YqLCXNB3yUuLxFNlXbl3W8gMyt3XtVqxLzpyj45GmokToGpsRe0GXDv0pHrPyQRx3hid7ViTQxx5IMWLrIE4jxASnL6FsD8KljM7qf4u2BhDOQDyfN8DcMvh8E73bfRYFULDqOzCSc1ThILU"
+              frameborder="0"
+              allowfullscreen
+              style="display: block;margin-top: 0;margin-left: auto;margin-right: auto;max-width: 100%;"
+            ></Iframe>
+          </div>
+        </section>
       </>
     );
   }
