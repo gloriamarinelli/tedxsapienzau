37b7f609b703e54f0f496431f010a10ed2126d96
Alessio Scanu <scanu.2129841@studenti.uniroma1.it>
2024-04-13 16:27:59 +0200

    update nome variabile wallpaper + valore strength

--- client/src/components/components/partners_scrolldown/ImageOne.js
+++ client/src/components/components/partners_scrolldown/ImageOne.js
@@ -1,18 +1,37 @@
-import { Parallax } from 'react-parallax';
-//import wallpaper from './img/test_2.jpg'
-import wallpaper from "./img/volunteers.webp"
-
+import { Parallax } from "react-parallax";
+import wallpaper from "./img/volunteers.webp";
 
 const ImageOne = () => (
-    <Parallax className='image' blur={0} color= "#191919" bgImage={wallpaper} strength={400} bgImageStyle={{minHeight:"100vh"}}>
-        <div className='content ct-right'>
-            <div className='text-div'>
-                <h3 className="img-title">TEDx, molto più che un evento.</h3>
-                <p className="img-txt">Realizzare un TEDx è un lavoro di squadra. L’organizzazione avviene in sinergia con i nostri partner.</p>
-                <p className="img-link"><a href="https://www.tedxsapienzau.com/" target="_blank" rel="noopener noreferrer">Scopri di più</a></p>
-            </div>
-        </div>
-    </Parallax>
+  <Parallax
+    className="image"
+    blur={0}
+    color="#191919"
+    bgImage={wallpaper}
+    strength={500}
+    bgImageStyle={{
+      minHeight: "100vh",
+      filter: "brightness(50%)",
+    }}
+  >
+    <div className="content ct-right">
+      <div className="text-div">
+        <h3 className="img-title">TEDx, molto più che un evento.</h3>
+        <p className="img-txt">
+          Realizzare un TEDx è un lavoro di squadra. L’organizzazione avviene in
+          sinergia con i nostri partner.
+        </p>
+        <p className="img-link">
+          <a
+            href="https://www.tedxsapienzau.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Scopri di più
+          </a>
+        </p>
+      </div>
+    </div>
+  </Parallax>
 );
 
-export default ImageOne
+export default ImageOne;
--- client/src/components/components/partners_scrolldown/ImageTwo.js
+++ client/src/components/components/partners_scrolldown/ImageTwo.js
@@ -1,17 +1,34 @@
-import { Parallax } from 'react-parallax';
-import Ocean from './img/header_blog23.webp'
+import { Parallax } from "react-parallax";
+import wallpaper from "./img/header_blog23.webp";
 const ImageTwo = () => (
-    <Parallax className='image' blur={0} bgImage={Ocean} strength={400} bgImageStyle={{minHeight:"100vh"}}>
-        <div className='content ct-left'>
-            <div className='text-div'>
-                <h3 className="img-title">Perché collaborare?</h3>
-                <h4 className="img-txt">Uniti per un obiettivo</h4>
-                <p className="img-txt-2">Circa 3 anni fa, dallo stimolo di sei studenti si è deciso di portare questo format internazionale nella nostra università Sapienza. La nostra è infatti l’Università più grande d’Europa, con più di 120 mila studenti iscritti.</p>
-                <p className="img-txt-2">Il nostro obiettivo è dare voce alle idee di valore che la nostra comunità accademica vuole far emergere.</p>
-                <p className="img-txt-2">Ora siamo in più di 120 volontari.</p>
-            </div>
-        </div>
-    </Parallax>
+  <Parallax
+    className="image"
+    blur={0}
+    bgImage={wallpaper}
+    strength={500}
+    bgImageStyle={{
+      minHeight: "100vh",
+      filter: "brightness(50%)",
+    }}
+  >
+    <div className="content ct-left">
+      <div className="text-div">
+        <h3 className="img-title">Perché collaborare?</h3>
+        <h4 className="img-txt">Uniti per un obiettivo</h4>
+        <p className="img-txt-2">
+          Circa 3 anni fa, dallo stimolo di sei studenti si è deciso di portare
+          questo format internazionale nella nostra università Sapienza. La
+          nostra è infatti l’Università più grande d’Europa, con più di 120 mila
+          studenti iscritti.
+        </p>
+        <p className="img-txt-2">
+          Il nostro obiettivo è dare voce alle idee di valore che la nostra
+          comunità accademica vuole far emergere.
+        </p>
+        <p className="img-txt-2">Ora siamo in più di 120 volontari.</p>
+      </div>
+    </div>
+  </Parallax>
 );
 
-export default ImageTwo
+export default ImageTwo;
