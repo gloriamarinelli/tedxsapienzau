6ed65319ce9fe5030ddcd0c9e070a0f6b151edf4
alessio <alessio.scanu01@gmail.com>
2025-02-27 12:32:34 +0100

    sorting partners sections

--- client/src/components/components/partners24/PartnersList24.jsx
+++ client/src/components/components/partners24/PartnersList24.jsx
@@ -18,6 +18,68 @@ export default function PartnersList24() {
   const currentLanguage = i18n.language;
   const lang_it = "it";
 
+  /* partners filtering depending on "partner_type" */
+  const mainPartners = partnersInfo.partners.filter(
+    (p) => p.partner_type === "main"
+  );
+  const supportingPartners = partnersInfo.partners.filter(
+    (p) => p.partner_type === "supporting"
+  );
+  const smartPartners = partnersInfo.partners.filter(
+    (p) => p.partner_type === "smart"
+  );
+  const friendlyPartners = partnersInfo.partners.filter(
+    (p) => p.partner_type === "friendly"
+  );
+  const communityPartners = partnersInfo.partners.filter(
+    (p) => p.partner_type === "community"
+  );
+  const patrocinioPartners = partnersInfo.partners.filter(
+    (p) => p.partner_type === "patrocinio"
+  );
+
+  /* grid style const */
+  const gridStyle = {
+    marginTop: "30px",
+    display: "grid",
+    gridTemplateColumns:
+      windowSize > global.UTILS.BIG_TABLET_WIDTH
+        ? "repeat(4, 1fr)" // desktop view (4 columns)
+        : windowSize > global.UTILS.TABLET_WIDTH
+        ? "repeat(2, 1fr)" // tablet view (2 columns)
+        : "repeat(1, 1fr)", // smartphone view (1 column)
+    gap:
+      windowSize > global.UTILS.BIG_TABLET_WIDTH
+        ? "100px" // desktop view
+        : windowSize > global.UTILS.TABLET_WIDTH
+        ? "50px" // tablet view
+        : "30px", // smartphone view
+    justifyContent: "center",
+  };
+
+  /* sections title style const */
+  const pTypeStyle = {
+    textTransform: "uppercase",
+    color: "white",
+    marginTop: "70px",
+    fontSize: windowSize > global.UTILS.TABLET_WIDTH ? "50px" : "6vw",
+  };
+
+  /* generate partners card */
+  const renderPartners = (partners) =>
+    partners.map((partner) => (
+      <PartnerCard24
+        key={partner.id}
+        name={partner.name}
+        imgName={partner.imgName}
+        descr={
+          currentLanguage === lang_it ? partner.descr_it : partner.descr_en
+        }
+        partner_type={partner.partner_type}
+        wb_link={partner.wb_link}
+      />
+    ));
+
   return (
     <div id="external_div">
       <div id="title_div">
@@ -31,37 +93,46 @@ export default function PartnersList24() {
           Partners
         </h1>
       </div>
-      <div
-        id="partners_div"
-        style={{
-          marginTop: "30px",
-          display: "grid",
-          gridTemplateColumns:
-            windowSize > global.UTILS.BIG_TABLET_WIDTH
-              ? "repeat(4, 1fr)" // desktop view (4 columns)
-              : windowSize > global.UTILS.TABLET_WIDTH
-              ? "repeat(2, 1fr)" // tablet view (2 columns)
-              : "repeat(1, 1fr)", // smartphone view (1 column)
-          gap:
-            windowSize > global.UTILS.BIG_TABLET_WIDTH
-              ? "100px" // desktop view
-              : windowSize > global.UTILS.TABLET_WIDTH
-              ? "50px" // tablet view
-              : "30px", // smartphone view
-          justifyContent: "center",
-        }}
-      >
-        {partnersInfo.partners.map((partner) => (
-          <PartnerCard24
-            key={partner.id}
-            name={partner.name}
-            imgName={partner.imgName}
-            descr={
-              currentLanguage == lang_it ? partner.descr_it : partner.descr_en
-            }
-            wb_link={partner.wb_link}
-          />
-        ))}
+
+      <div id="ext_main_div">
+        <h2 style={pTypeStyle}>Main Partners</h2>
+        <div id="int_main_div" style={gridStyle}>
+          {renderPartners(mainPartners)}
+        </div>
+      </div>
+
+      <div id="ext_supporting_div">
+        <h2 style={pTypeStyle}>Supporting Partners</h2>
+        <div id="int_supporting_div" style={gridStyle}>
+          {renderPartners(supportingPartners)}
+        </div>
+      </div>
+
+      <div id="ext_smart_div">
+        <h2 style={pTypeStyle}>Smart Partners</h2>
+        <div id="int_smart_div" style={gridStyle}>
+          {renderPartners(smartPartners)}
+        </div>
+      </div>
+      <div id="ext_friendly_div">
+        <h2 style={pTypeStyle}>Friendly Partners</h2>
+        <div id="int_friendly_div" style={gridStyle}>
+          {renderPartners(friendlyPartners)}
+        </div>
+      </div>
+
+      <div id="ext_community_div">
+        <h2 style={pTypeStyle}>Community Partners</h2>
+        <div id="int_community_div" style={gridStyle}>
+          {renderPartners(communityPartners)}
+        </div>
+      </div>
+
+      <div id="ext_patrocinio_div">
+        <h2 style={pTypeStyle}>Patrocinio Partners</h2>
+        <div id="int_patrocinio_div" style={gridStyle}>
+          {renderPartners(patrocinioPartners)}
+        </div>
       </div>
     </div>
   );
