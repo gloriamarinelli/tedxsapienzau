efad90b5721b80217d96fa4535da9ed8473a3c5e
Alessio Scanu <105634427+alessioscanu@users.noreply.github.com>
2025-03-31 14:52:32 +0200

    Revert "update sponsors layout"
    This reverts commit bf8da870362e5f0cb71ba0f464512eec95d3444f.

--- client/src/components/components/partners24/PartnersList24.jsx
+++ client/src/components/components/partners24/PartnersList24.jsx
@@ -49,29 +49,12 @@ export default function PartnersList24() {
   );
 
   /* grid style const */
-  const mainGridStyle = {
-    marginTop: "30px",
-    display: windowSize > global.UTILS.BIG_TABLET_WIDTH ? "flex" : "grid", // desktop view
-    gridTemplateColumns:
-      windowSize > global.UTILS.TABLET_WIDTH
-        ? "repeat(2, 1fr)" // tablet view (2 columns)
-        : "repeat(1, 1fr)", // smartphone view (1 column)
-    gap:
-      windowSize > global.UTILS.BIG_TABLET_WIDTH
-        ? "100px" // desktop view
-        : windowSize > global.UTILS.TABLET_WIDTH
-        ? "50px" // tablet view
-        : "30px", // smartphone view
-    justifyContent: "center",
-  };
-
-  /* grid style const */
-  const secondaryGridStyle = {
+  const gridStyle = {
     marginTop: "30px",
     display: "grid",
     gridTemplateColumns:
       windowSize > global.UTILS.BIG_TABLET_WIDTH
-        ? "repeat(5, 1fr)" // desktop view ( columns)
+        ? "repeat(4, 1fr)" // desktop view (4 columns)
         : windowSize > global.UTILS.TABLET_WIDTH
         ? "repeat(2, 1fr)" // tablet view (2 columns)
         : "repeat(1, 1fr)", // smartphone view (1 column)
@@ -89,21 +72,6 @@ export default function PartnersList24() {
     color: "white",
     marginTop: "70px",
     fontSize: windowSize > global.UTILS.TABLET_WIDTH ? "50px" : "6vw",
-  };
-
-  /* group div flex style const */
-  const groupDivFlexStyle = {
-    display: "flex",
-    flexWrap: "wrap",
-    gap: windowSize > global.UTILS.BIG_TABLET_WIDTH ? "100px" : "",
-    justifyContent: "center",
-  };
-
-  /* group div grid style const */
-  const groupDivmainGridStyle = {
-    display: "grid",
-    gridTemplateColumns: "repeat(5, 1fr)",
-    marginTop: "30px",
   };
 
   /* generate partners card */
@@ -134,73 +102,75 @@ export default function PartnersList24() {
           Sponsors
         </h1>
       </div>
-      <div id="list_div">
-        <div id="main_&_supporting_&_tech" style={groupDivFlexStyle}>
-          <div id="ext_main_div">
-            <h2 style={pTypeStyle}>Main Sponsors</h2>
-            <div id="int_main_div" style={mainGridStyle}>
-              {renderPartners(mainPartners)}
-            </div>
-          </div>
-
-          <div id="ext_supporting_div">
-            <h2 style={pTypeStyle}>Supporting Sponsors</h2>
-            <div id="int_supporting_div" style={mainGridStyle}>
-              {renderPartners(supportingPartners)}
-            </div>
-          </div>
-
-          <div id="ext_technical_div">
-            <h2 style={pTypeStyle}>Technical Sponsors</h2>
-            <div id="int_technical_div" style={mainGridStyle}>
-              {renderPartners(technicalPartners)}
-            </div>
+      <div
+        id="list_div"
+        /*  
+        style={{
+          display: windowSize > global.UTILS.BIG_TABLET_WIDTH ? "grid" : "",
+          gridTemplateColumns:
+            windowSize > global.UTILS.BIG_TABLET_WIDTH ? "repeat(2, 1fr)" : "",
+          marginLeft: windowSize > global.UTILS.BIG_TABLET_WIDTH ? "100px" : "",
+        }}*/
+      >
+        <div id="ext_main_div">
+          <h2 style={pTypeStyle}>Main Sponsors</h2>
+          <div id="int_main_div" style={gridStyle}>
+            {renderPartners(mainPartners)}
           </div>
         </div>
 
-        <div id="smart_&_friendly" style={groupDivFlexStyle}>
-          <div id="ext_smart_div">
-            <h2 style={pTypeStyle}>Smart Sponsors</h2>
-            <div id="int_smart_div" style={mainGridStyle}>
-              {renderPartners(smartPartners)}
-            </div>
-          </div>
-
-          <div id="ext_friendly_div">
-            <h2 style={pTypeStyle}>Friendly Sponsors</h2>
-            <div id="int_friendly_div" style={mainGridStyle}>
-              {renderPartners(friendlyPartners)}
-            </div>
+        <div id="ext_supporting_div">
+          <h2 style={pTypeStyle}>Supporting Sponsors</h2>
+          <div id="int_supporting_div" style={gridStyle}>
+            {renderPartners(supportingPartners)}
           </div>
         </div>
 
-        <div id="others" style={groupDivFlexStyle}>
-          <div id="ext_generic_div">
-            <h2 style={pTypeStyle}>Partners</h2>
-            <div id="int_generic_div" style={secondaryGridStyle}>
-              {renderPartners(genericPartners)}
-            </div>
+        <div id="ext_technical_div">
+          <h2 style={pTypeStyle}>Technical Sponsors</h2>
+          <div id="int_technical_div" style={gridStyle}>
+            {renderPartners(technicalPartners)}
           </div>
+        </div>
 
-          <div id="ext_community_div">
-            <h2 style={pTypeStyle}>Community Partners</h2>
-            <div id="int_community_div" style={secondaryGridStyle}>
-              {renderPartners(communityPartners)}
-            </div>
+        <div id="ext_smart_div">
+          <h2 style={pTypeStyle}>Smart Sponsors</h2>
+          <div id="int_smart_div" style={gridStyle}>
+            {renderPartners(smartPartners)}
           </div>
-
-          <div id="ext_patrocinio_div">
-            <h2 style={pTypeStyle}>{t("partners_list.patrocinio_div")}</h2>
-            <div id="int_patrocinio_div" style={secondaryGridStyle}>
-              {renderPartners(patrocinioPartners)}
-            </div>
+        </div>
+        <div id="ext_friendly_div">
+          <h2 style={pTypeStyle}>Friendly Sponsors</h2>
+          <div id="int_friendly_div" style={gridStyle}>
+            {renderPartners(friendlyPartners)}
           </div>
+        </div>
 
-          <div id="ext_patrocinioEP_div">
-            <h2 style={pTypeStyle}>{t("partners_list.patrocinioEP_div")}</h2>
-            <div id="int_patrocinioEP_div" style={secondaryGridStyle}>
-              {renderPartners(patrocinioEPPartners)}
-            </div>
+        <div id="ext_generic_div">
+          <h2 style={pTypeStyle}>Partners</h2>
+          <div id="int_generic_div" style={gridStyle}>
+            {renderPartners(genericPartners)}
+          </div>
+        </div>
+
+        <div id="ext_community_div">
+          <h2 style={pTypeStyle}>Community Partners</h2>
+          <div id="int_community_div" style={gridStyle}>
+            {renderPartners(communityPartners)}
+          </div>
+        </div>
+
+        <div id="ext_patrocinio_div">
+          <h2 style={pTypeStyle}>{t("partners_list.patrocinio_div")}</h2>
+          <div id="int_patrocinio_div" style={gridStyle}>
+            {renderPartners(patrocinioPartners)}
+          </div>
+        </div>
+
+        <div id="ext_patrocinioEP_div">
+          <h2 style={pTypeStyle}>{t("partners_list.patrocinioEP_div")}</h2>
+          <div id="int_patrocinioEP_div" style={gridStyle}>
+            {renderPartners(patrocinioEPPartners)}
           </div>
         </div>
       </div>
